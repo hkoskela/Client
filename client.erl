ec6e80b98d40a00310aa568a0729b28328621863
Henry Koskela <henry.koskela@gmail.com>
2015-03-30 19:55:20 +0300

    hep

--- client.erl
+++ client.erl
@@ -1,8 +1,8 @@
 -module(client).
 -define(SERVER_NODE, 'pi@192.168.2.102').
 -define(PROGRAM_TO_UPDATE, 'hello').
--export([start/0,loop/0,update/0]).
--vsn(1.42).
+-export([start/0,loop/0,update/0,init/0]).
+-vsn(1.50).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -12,6 +12,24 @@ start() ->
 	register(client,Pid).
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+init() ->
+	{ok,{client,[C]}} = beam_lib:version(client),
+	{ok,{hello,[L]}} = beam_lib:version(?PROGRAM_TO_UPDATE),
+	{server,?SERVER_NODE} ! {self(), node(), beam_lib:version(?PROGRAM_TO_UPDATE), beam_lib:version(client)},
+	receive
+		{{ok,{hello,[V]}},{ok,{client,[Sc]}}} ->
+			io:format("***CLIENT (~p)***~n", [C]),
+			io:format("Hello.beam  Local: ~p Server: ~p~n",[L,V]),
+			io:format("Client.beam Local: ~p Server: ~p~n",[C,Sc])
+	after 
+		15000 ->
+			ok
+	end,
+	?MODULE:loop().
+	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+
 
 update() ->
 
