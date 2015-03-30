dd5400f57396ec1d285f612bdd7b4f56a5cb38ef
Henry Koskela <henry.koskela@gmail.com>
2015-03-30 13:53:15 +0300

    2

--- client.erl
+++ client.erl
@@ -1,27 +1,28 @@
 -module(client).
 -define(SERVER_NODE, 'pi@192.168.2.102').
 -define(PROGRAM_TO_UPDATE, 'hello').
--export([start/0,loop/0,update/0,init/0]).
+-export([start/0,loop/0,update/0]).
 -vsn(1.21).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 start() ->
 
-    spawn(?MODULE,init,[]).
+    spawn(?MODULE,loop,[]).
+	
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	
-init() ->
-	{ok,{client,[C]}} = beam_lib:version(client),
-	io:format("*** CLIENT (~p) *** pinging server ~p~n",[C,?SERVER_NODE]),
-    case net_adm:ping(?SERVER_NODE) of
-		pong ->
-			?MODULE:loop();
-		pang ->
-			timer:sleep(10000),
-			?MODULE:start()
-	end.
+%%init() ->
+%%	{ok,{client,[C]}} = beam_lib:version(client),
+%%	io:format("*** CLIENT (~p) *** pinging server ~p~n",[C,?SERVER_NODE]),
+%%	case net_adm:ping(?SERVER_NODE) of
+%%		pong ->
+%%			?MODULE:loop();
+%%		pang ->
+%%			timer:sleep(10000),
+%%			?MODULE:start()
+%%	end.
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -41,7 +42,7 @@ loop() ->
     
 	{server,?SERVER_NODE} ! {self(), node(), beam_lib:version(?PROGRAM_TO_UPDATE)},
     receive
-        {ok,{hello,[V]}} ->
+        {{ok,{hello,[V]}}} ->
 			io:format("***CLIENT (~p)***~n", [C]),
 		   	io:format("Local: ~p Server: ~p~n",[L,V]);
 		other ->
