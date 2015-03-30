9decca3585208515898035c1f789c9e3cf42a361
Henry Koskela <henry.koskela@gmail.com>
2015-03-30 19:33:08 +0300

    hep

--- client.erl
+++ client.erl
@@ -2,13 +2,14 @@
 -define(SERVER_NODE, 'pi@192.168.2.102').
 -define(PROGRAM_TO_UPDATE, 'hello').
 -export([start/0,loop/0,update/0]).
--vsn(1.33).
+-vsn(1.40).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 start() ->
 
-    spawn(?MODULE,loop,[]).
+    Pid = spawn(?MODULE,loop,[]),
+	register(client,Pid).
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -20,23 +21,27 @@ update() ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 loop() ->
-
+    net_adm:ping(?SERVER_NODE),
+	?MODULE:update(),
     {ok,{client,[C]}} = beam_lib:version(client),
 	{ok,{hello,[L]}} = beam_lib:version(?PROGRAM_TO_UPDATE),
-	io:format("*** CLIENT (~p)*** sending version information to ~p~n",[C,?SERVER_NODE]),
-    
-	{server,?SERVER_NODE} ! {self(), node(), beam_lib:version(?PROGRAM_TO_UPDATE), beam_lib:version(client)},
-    receive
-        {{ok,{hello,[V]}},{ok,{client,[Sc]}}} ->
-			io:format("***CLIENT (~p)***~n", [C]),
-		   	io:format("Hello.beam  Local: ~p Server: ~p~n",[L,V]),
-			io:format("Client.beam Local: ~p Server: ~p~n",[C,Sc])
-    after
-       15000 ->
-            {server,?SERVER_NODE} ! {node(),"UpdateMe",beam_lib:version(client)},
-			io:format("*** CLIENT (~p)*** no response~n",[C])
-    end,
-    ?MODULE:update(),
-	timer:sleep(60000),
+	 
+	receive
+		{"request_version"} ->
+			{server,?SERVER_NODE} ! {self(), node(), beam_lib:version(?PROGRAM_TO_UPDATE), beam_lib:version(client)},
+			receive
+				{{ok,{hello,[V]}},{ok,{client,[Sc]}}} ->
+					io:format("***CLIENT (~p)***~n", [C]),
+					io:format("Hello.beam  Local: ~p Server: ~p~n",[L,V]),
+					io:format("Client.beam Local: ~p Server: ~p~n",[C,Sc])
+			after 
+				5000 ->
+					ok
+			end
+	after
+		5000 ->
+			ok
+	end,
+	timer:sleep(8000),
     ?MODULE:loop().
     
