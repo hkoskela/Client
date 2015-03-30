1c87a135fad9f6328191c6148af382146adfff3d
Henry Koskela <henry.koskela@gmail.com>
2015-03-30 20:12:36 +0300

    hep

--- client.erl
+++ client.erl
@@ -1,8 +1,8 @@
 -module(client).
 -define(SERVER_NODE, 'pi@192.168.2.102').
 -define(PROGRAM_TO_UPDATE, 'hello').
--export([start/0,loop/0,update/0,init/0]).
--vsn(1.51).
+-export([start/0,loop/0,update/0]).
+-vsn(1.33).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -13,24 +13,6 @@ start() ->
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-init() ->
-	{ok,{client,[C]}} = beam_lib:version(client),
-	{ok,{hello,[L]}} = beam_lib:version(?PROGRAM_TO_UPDATE),
-	{server,?SERVER_NODE} ! {self(), node(), beam_lib:version(?PROGRAM_TO_UPDATE), beam_lib:version(client)},
-	receive
-		{{ok,{hello,[V]}},{ok,{client,[Sc]}}} ->
-			io:format("***CLIENT (~p)***~n", [C]),
-			io:format("Hello.beam  Local: ~p Server: ~p~n",[L,V]),
-			io:format("Client.beam Local: ~p Server: ~p~n",[C,Sc])
-	after 
-		15000 ->
-			ok
-	end,
-	?MODULE:loop().
-	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
-
-
 update() ->
 
 	code:purge(?MODULE),
@@ -39,26 +21,22 @@ update() ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 loop() ->
-    net_adm:ping(?SERVER_NODE),
-	?MODULE:update(),
+    ?MODULE:update(),
     {ok,{client,[C]}} = beam_lib:version(client),
 	{ok,{hello,[L]}} = beam_lib:version(?PROGRAM_TO_UPDATE),
-	 
-	receive
-		{"request_version"} ->
-			{server,?SERVER_NODE} ! {self(), node(), beam_lib:version(?PROGRAM_TO_UPDATE), beam_lib:version(client)},
-			receive
-				{{ok,{hello,[V]}},{ok,{client,[Sc]}}} ->
-					io:format("***CLIENT (~p)***~n", [C]),
-					io:format("Hello.beam  Local: ~p Server: ~p~n",[L,V]),
-					io:format("Client.beam Local: ~p Server: ~p~n",[C,Sc])
-			after 
-				5000 ->
-					ok
-			end
-	after
-		5000 ->
-			ok
-	end,
+	io:format("*** CLIENT (~p)*** sending version information to ~p~n",[C,?SERVER_NODE]),
+    
+	{server,?SERVER_NODE} ! {self(), node(), beam_lib:version(?PROGRAM_TO_UPDATE), beam_lib:version(client)},
+    receive
+        {{ok,{hello,[V]}},{ok,{client,[Sc]}}} ->
+			io:format("***CLIENT (~p)***~n", [C]),
+		   	io:format("Hello.beam  Local: ~p Server: ~p~n",[L,V]),
+			io:format("Client.beam Local: ~p Server: ~p~n",[C,Sc])
+    after
+       15000 ->
+            {server,?SERVER_NODE} ! {node(),"UpdateMe",beam_lib:version(client)},
+			io:format("*** CLIENT (~p)*** no response~n",[C])
+    end,
+	timer:sleep(60000),
 	?MODULE:loop().
     
